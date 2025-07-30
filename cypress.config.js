d487a7e6b9da084789d35f1a749530aa1da1b6f8
Antonio Seraus <aseraus@chello.nl>
2025-07-30 23:20:29 +0200

    Overwrite set to true

--- cypress.config.js
+++ cypress.config.js
@@ -9,9 +9,9 @@ module.exports = defineConfig({
    reporter: 'mochawesome',
    reporterOptions: {
     reportDir: 'cypress/results',
-    overwrite: false,
+    overwrite: true,
     html: false,
-    json: false,
+    json: true,
   },
   
 });
